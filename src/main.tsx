c7d738d02b048d5ab76c048265655e668b1acbab
blasecato <bl.calderon@gmail.com>
2024-11-14 17:39:08 -0500

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./main.scss";
-import Router from './Router.tsx'
+import Router from './Router'
 import { Provider } from 'react-redux';
-import store from './services/store.ts';
+import store from './services/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
